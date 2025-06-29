012f50ed3c577376812c253ea37776e02c9e8fce
iconic-coder <mbatiasir@gmail.com>
2025-06-29 13:43:33 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Declare customerName to be 'bob' in global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because leastFavoriteCustomer is a const
+  leastFavoriteCustomer = 'someone else';
+}
